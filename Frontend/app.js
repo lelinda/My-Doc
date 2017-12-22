7aff3ec4e70462c14305c6cef989a5dd27fb026e
Darlene <hi.darlenekim@gmail.com>
2017-12-22 15:48:07 -0800

    Front End Development so far!

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,48 +1,48 @@
 var app = angular.module("myApp", ["ui.router"])
 
-app.config(function ($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise("/");
 
-  // home view -> login
-  $stateProvider
-    .state("home", {
-      url: "/",
-      templateUrl: "./views/home.html",
-    })
+    // home view -> login
+    $stateProvider
+        .state("home", {
+            url: "/",
+            templateUrl: "./views/home.html",
+        })
 
     // create new user
-    .state("userCreate", { 
-      url: "/user/new",
-      templateUrl: "./views/login-form.html",
-      controller: "userController"
+    .state("userCreate", {
+        url: "/user/new",
+        templateUrl: "./views/login-form.html",
+        controller: "userController"
     })
 
     // show user by ID
     .state("user", {
-      url: "/user/:id",
-      templateUrl: "./views/user.html",
-      controller: "userController"
-    })
-    // update user by ID
-    .state("userUpdate", {
-      url: "/user/:id/edit",
-      templateUrl: "./views/login-form.html",
-      controller: "userController"
-    })
+            url: "/user/:id",
+            templateUrl: "./views/user.html",
+            controller: "userController"
+        })
+        // update user by ID
+        .state("userUpdate", {
+            url: "/user/:id/edit",
+            templateUrl: "./views/login-form.html",
+            controller: "userController"
+        })
 
     // show search bar for doctors -> search doctors view
     .state("doctors", {
-      url: "/doctors",
-      templateUrl: "./views/doctors.html",
-      controller: "doctorController"
+        url: "/doctors",
+        templateUrl: "./views/doctors.html",
+        controller: "doctorController"
     })
 
     // show doctor by ID
     .state("doctor", {
-      url: "/doctors/:id",
-      templateUrl: "./views/doctor.html",
-      controller: "doctorController"
+        url: "/doctors/:id",
+        templateUrl: "./views/doctor.html",
+        controller: "doctorController"
     })
 
 })
