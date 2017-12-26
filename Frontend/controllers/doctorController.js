bbf24e9dfa2fbb750d6148ea51684d71df12de2c
ryan <ryfield12@gmail.com>
2017-12-25 23:21:07 -0800

    google maps pops up in doctor search view after you submit a search

--- Frontend/controllers/doctorController.js
+++ Frontend/controllers/doctorController.js
@@ -1,64 +1,86 @@
 app.controller("doctorController", function ($scope, $state, $stateParams, $http, doctorService) {
-
   // doctor search function
+  var map = null;
+  $scope.locationRequired = true;
   $scope.getDoctors = function () {
-    var lat = "";
-    var lng = "";
-    var name = "";
-    var specialty = "";
-    if ($scope.searchedName != undefined){
-      var name = $scope.searchedName.split(" ").join("%20")
-    };
-    if ($scope.searchedSpecialty != undefined){
-    specialty = $scope.searchedSpecialty.split(" ").join("-")
-    };
-    if($scope.searchedLocation !== undefined){
-      doctorService.geoMap($scope.searchedLocation)
-      .then(function(response){
-        console.log(response)
-        lat = response.data.results[0].geometry.location.lat;
-        lng = response.data.results[0].geometry.location.lng;
-        console.log(lat, lng)
-      })
-      .catch(function(error){
-        console.log(error)
-      })
+    if ($scope.searchedLocation == undefined) {
+      console.log("Empty")
+      $scope.locationRequired = false;
+    } else {
+      
+      console.log("initMap end")
+      $scope.locationRequired = true;
+      var lat = "";
+      var lng = "";
+      var name = "";
+      var specialty = "";
+      if ($scope.searchedName != undefined) {
+        var name = $scope.searchedName.split(" ").join("%20")
+      };
+      if ($scope.searchedSpecialty != undefined) {
+        specialty = $scope.searchedSpecialty.split(" ").join("-")
+      };
+      if ($scope.searchedLocation !== undefined) {
+        doctorService.geoMap($scope.searchedLocation)
+          .then(function (response) {
+            console.log(response)
+            lat = response.data.results[0].geometry.location.lat;
+            lng = response.data.results[0].geometry.location.lng;
+            console.log(lat, lng)
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+      }
+      var coords = "";
+      setTimeout(function () { coords = lat + "%2C" + lng; }, 500);
+      setTimeout(function () {
+        doctorService.getDoctorsApi(name, specialty, coords)
+          .then(function (response) {
+            console.log("Doctors Response: ", response);
+            console.log("Doctors Data:", response.data.data);
+            $scope.doctors = response.data.data;
+            // Changing values from "male" to "Male", "female" to "Female", true to "Yes", false to "No"
+            var femaleVal = "Female"
+            var maleVal = "Male"
+            var yes = "Yes"
+            var no = "No"
+            for (var i = 0; i < response.data.data.length; i++) {
+              if (response.data.data[i].profile.gender === "female") {
+                response.data.data[i].profile.gender = femaleVal;
+              }
+              if (response.data.data[i].profile.gender === "male") {
+                response.data.data[i].profile.gender = maleVal;
+              }
+              if (response.data.data[i].practices[0].accepts_new_patients === true) {
+                response.data.data[i].practices[0].accepts_new_patients = yes;
+              }
+              if (response.data.data[i].practices[0].accepts_new_patients === false) {
+                response.data.data[i].practices[0].accepts_new_patients = no;
+              }
+            }
+              map = new google.maps.Map(document.getElementById('mapMain'), {
+                zoom: 8,
+                center: new google.maps.LatLng(lat, lng)
+          
+                
+              });
+              for (var i = 0; i < response.data.data.length; i++){
+                console.log(response.data.data[i].practices[0].lat + " " + response.data.data[i].practices[0].lng)
+                var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(response.data.data[i].practices[0].lat, response.data.data[i].practices[0].lon),
+                map: map
+                })
+              }
+              console.log("initMap end")
+          }, function (error) {
+            console.log(error);
+          })
+          
+      }, 500);
     }
-    var coords = "";
-    setTimeout(function(){coords = lat + "%2C" + lng;}, 1000);
-    setTimeout(function(){
-    doctorService.getDoctorsApi(name, specialty, coords)
-      .then(function (response) {
-        console.log("Doctors Response: ", response);
-        console.log("Doctors Data:", response.data.data);
-        $scope.doctors = response.data.data;
-        // Changing values from "male" to "Male", "female" to "Female", true to "Yes", false to "No"
-        var femaleVal = "Female"
-        var maleVal = "Male"
-        var yes = "Yes"
-        var no = "No"
-        for (var i = 0; i < response.data.data.length; i++) {
-          if (response.data.data[i].profile.gender === "female") {
-            response.data.data[i].profile.gender = femaleVal;
-          }
-          if (response.data.data[i].profile.gender === "male") {
-            response.data.data[i].profile.gender = maleVal;
-          }
-          if (response.data.data[i].practices[0].accepts_new_patients === true) {
-            response.data.data[i].practices[0].accepts_new_patients = yes;
-          }
-          if (response.data.data[i].practices[0].accepts_new_patients === false) {
-            response.data.data[i].practices[0].accepts_new_patients = no;
-          }
-        }
-      }, function (error) {
-        console.log(error);
-      })
-    }, 2000);
+    
   }
-
-  // $scope.getDoctors();
-
   // Get one doctor by Id
   if ($stateParams.id == "" || $stateParams.id == undefined || $stateParams.id == null) {
     doctorService.getDoctorById($stateParams.id, function (doctor) {
--- Frontend/services/doctorService.js
+++ Frontend/services/doctorService.js
@@ -1,14 +1,14 @@
-app.service("doctorService", function($http) {
-//  doctor search
-  this.getDoctorsApi = function(name, specialty, coords){
+app.service("doctorService", function ($http) {
+  //  doctor search
+  this.getDoctorsApi = function (name, specialty, coords) {
     console.log(name + " and " + specialty + " " + coords);
-    return $http.get("http://localhost:5000/api/doctor?name=" + name + "&specialty_uid=" + specialty + "&location=" + coords + "&skip=0&limit=10&user_key=efe95df06a0afcd63f78c2b81c58fb4c")
+    return $http.get("http://localhost:5000/api/doctor?name=" + name + "&specialty_uid=" + specialty + "&location=" + coords)
   }
 
   var _location = ""; //address for geocode
   // google maps geocode call
-    this.geoMap = function (searchedLocation) {
-      return $http.get("https://maps.googleapis.com/maps/api/geocode/json?", {
+  this.geoMap = function (searchedLocation) {
+    return $http.get("https://maps.googleapis.com/maps/api/geocode/json?", {
       params: {
         address: searchedLocation,
         key: 'AIzaSyABBleRUqFda2vbGC2oDLFT7z46fCnYfow'
@@ -16,8 +16,8 @@ app.service("doctorService", function($http) {
     });
 
   }
-    // Get one by Id
-  this.getDoctorById = function(id, cb) {
+  // Get one by Id
+  this.getDoctorById = function (id, cb) {
     if (id == "" || id == undefined || id == null) {
       var doctor = {}
       cb(doctor)
@@ -25,11 +25,10 @@ app.service("doctorService", function($http) {
     else {
       $http.get("http://localhost:5000/api/doctor/" + id + "/")
         .then(function (response) {
-        cb(response.data)
-      }, function (error) {
-        console.log(error);
-      })
+          cb(response.data)
+        }, function (error) {
+          console.log(error);
+        })
     }
   }
-  })
-  
+})
